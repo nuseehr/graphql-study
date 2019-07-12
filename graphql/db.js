f96cad959f966fd04af990da017dd0c2797e4222
Lee Sun Hong <nuseehr@naver.com>
2019-07-12 18:12:09 +0900

    Wrapping a REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,65 +1,18 @@
-let movies = [
-  {
-    id: "1",
-    name: "John Wick",
-    score: 4.0,
-  },
-  {
-    id: "2",
-    name: "Guardians Of Galaxy",
-    score: 4.5,
-  },
-  {
-    id: "3",
-    name: "Toy Story",
-    score: 3.0,
-  },
-  {
-    id: "4",
-    name: "Baby Driver",
-    score: 3.5,
-  },
-  {
-    id: "5",
-    name: "Dunkirk",
-    score: 5.0,
-  },
-  {
-    id: "6",
-    name: "Okja",
-    score: 3.5,
-  },
-  {
-    id: "7",
-    name: "Living Boy In New York",
-    score: 1.5,
+import fetch from 'node-fetch';
+const API_URL = "https://yts.lt/api/v2/list_movies.json?";
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+
+  if(limit > 0) {
+    REQUEST_URL += `limit=${limit}`; 
   }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredMovies = movies.filter(m => m.id === String(id));
-  return filteredMovies[0];
-}
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter(m => m.id !== String(id));
-
-  if(movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+  if(rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
   }
-}
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
+  return fetch(REQUEST_URL)
+  .then(res => res.json())
+  .then(json => json.data.movies);
 };
+
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,8 @@
-import { getById, getMovies, addMovie } from './db';
+import { getMovies } from './db';
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, args) => getById(args.id)
-  },
-  Mutation: {
-    addMovie: (_, {name, score}) => addMovie(name, score)
+    movies: (_, {limit, rating}) => getMovies(limit, rating)
   }
 };
 
